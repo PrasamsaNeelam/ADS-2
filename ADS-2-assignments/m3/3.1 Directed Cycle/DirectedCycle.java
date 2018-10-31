87a191da1c98bad3124c09d53f0ab97a4d3fb0b8
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-10-31 22:19:35 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m3/3.1 Directed Cycle/DirectedCycle.java
+++ ADS-2-assignments/m3/3.1 Directed Cycle/DirectedCycle.java
@@ -1,37 +1,59 @@
+/**
+ * Class for directed cycle.
+ */
 public class DirectedCycle {
-    private boolean[] marked;        // marked[v] = has vertex v been marked?
-    private int[] edgeTo;            // edgeTo[v] = previous vertex on path to v
-    private boolean[] onStack;       // onStack[v] = is vertex on the stack?
-    private Stack<Integer> cycle;    // directed cycle (or null if no such cycle)
+    /**
+     * marked[v] = has vertex v been marked?.
+     */
+    private boolean[] marked;
+    /**
+     * edgeTo[v] = previous vertex on path to v.
+     */
+    private int[] edgeTo;
+    /**
+     * onStack[v] = is vertex on the stack?.
+     */
+    private boolean[] onStack;
+    /**
+     * directed cycle (or null if no such cycle).
+     */
+    private Stack<Integer> cycle;
 
     /**
-     * Determines whether the digraph {@code G} has a directed cycle and, if so,
+     * Determines whether the digraph {@code G}
+     * has a directed cycle and, if so,
      * finds such a cycle.
-     * @param G the digraph
+     * @param grp the digraph
      */
-    public DirectedCycle(Digraph G) {
-        marked  = new boolean[G.V()];
-        onStack = new boolean[G.V()];
-        edgeTo  = new int[G.V()];
-        for (int v = 0; v < G.V(); v++)
-            if (!marked[v] && cycle == null) dfs(G, v);
+    public DirectedCycle(final Digraph grp) {
+        marked  = new boolean[grp.vertex()];
+        onStack = new boolean[grp.vertex()];
+        edgeTo  = new int[grp.vertex()];
+        for (int v = 0; v < grp.vertex(); v++) {
+            if (!marked[v] && cycle == null) {
+                dfs(grp, v);
+            }
+        }
     }
 
-    // check that algorithm computes either the topological order or finds a directed cycle
-    private void dfs(Digraph G, int v) {
+    /**
+     * check that algorithm computes either the
+     * topological order or finds a directed cycle.
+     *
+     * @param      grp   The group
+     * @param      v     { parameter_description }
+     */
+    private void dfs(final Digraph grp, final int v) {
         onStack[v] = true;
         marked[v] = true;
-        for (int w : G.adj(v)) {
-            if (cycle != null) return;
-
-            // found new vertex, so recur
-            else if (!marked[w]) {
+        for (int w : grp.adj(v)) {
+            // short circuit if directed cycle found
+            if (cycle != null) {
+                return;
+            } else if (!marked[w]) {
                 edgeTo[w] = v;
-                dfs(G, w);
-            }
-
-            // trace back directed cycle
-            else if (onStack[w]) {
+                dfs(grp, w);
+            } else if (onStack[w]) {
                 cycle = new Stack<Integer>();
                 for (int x = v; x != w; x = edgeTo[x]) {
                     cycle.push(x);
@@ -46,33 +68,44 @@ public class DirectedCycle {
 
     /**
      * Does the digraph have a directed cycle?
-     * @return {@code true} if the digraph has a directed cycle, {@code false} otherwise
+     * @return {@code true} if the digraph has
+     * a directed cycle, {@code false} otherwise
      */
     public boolean hasCycle() {
         return cycle != null;
     }
 
     /**
-     * Returns a directed cycle if the digraph has a directed cycle, and {@code null} otherwise.
-     * @return a directed cycle (as an iterable) if the digraph has a directed cycle,
+     * Returns a directed cycle if the digraph has.
+     * a directed cycle, and {@code null} otherwise.
+     * @return a directed cycle (as an iterable) if
+     * the digraph has a directed cycle,
      *    and {@code null} otherwise
      */
     public Iterable<Integer> cycle() {
         return cycle;
     }
 
-    // certify that digraph has a directed cycle if it reports one
+    /**
+     * certify that digraph has a directed.
+     * cycle if it reports one
+     *
+     * @return     { description_of_the_return_value }
+     */
     private boolean check() {
-
         if (hasCycle()) {
             // verify cycle
             int first = -1, last = -1;
             for (int v : cycle()) {
-                if (first == -1) first = v;
+                if (first == -1) {
+                    first = v;
+                }
                 last = v;
             }
             if (first != last) {
-                System.err.printf("cycle begins with %d and ends with %d\n", first, last);
+                System.err.printf(
+                    "cycle begins with %d andends with %d\n",
+                    first, last);
                 return false;
             }
         }
--- ADS-2-assignments/m3/3.1 Directed Cycle/Stack.java
+++ ADS-2-assignments/m3/3.1 Directed Cycle/Stack.java
@@ -1,13 +1,63 @@
+/*************************************************************************
+ *  Compilation:  javac Stack.java
+ *  Execution:    java Stack < input.txt
+ *
+ *  A generic stack, implemented using a linked list. Each stack
+ *  element is of type Item.
+ *  % more tobe.txt
+ *  to be or not to - be - - that - - - is
+ *
+ *  % java Stack < tobe.txt
+ *  to be not that or be (2 left on stack)
+ *
+ *************************************************************************/
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class Stack<Item> implements Iterable<Item> {
-    private int n;          // size of the stack
-    private Node first;     // top of stack
 
-    // helper linked list class
+/**
+ *  The <tt>Stack</tt> class represents a last-in-first-out.
+ *   (LIFO) stack of generic items.
+ *  It supports the usual <em>push</em> and.
+ *   <em>pop</em> operations, along with methods.
+ *  for peeking at the top item, testing if the.
+ *   stack is empty, and iterating through.
+ *  the items in LIFO order.
+ *  <p>
+ *  All stack operations except iteration are constant time.
+ *  <p>
+ *  For additional documentation, see .
+ *  <a href="/algs4/13stacks">Section 1.3</a> of.
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ */
+/**
+ * List of .
+ *
+ * @param      <Item>  The item
+ */
+public class Stack<Item> implements Iterable<Item> {
+    /**
+     * integer n.
+     */
+    private int n;          // size of the stack
+    /**
+     * first node initialization.
+     */
+    private Node first;     // top of stack
+    /**
+     * Class for node.
+     * helper linked list class.
+     *
+     */
     private class Node {
+        /**
+         * Item type intialization.
+         */
         private Item item;
+        /**
+         * next Node type is created.
+         */
         private Node next;
     }
 
@@ -21,6 +71,7 @@ public class Stack<Item> implements Iterable<Item> {
 
    /**
      * Is the stack empty?
+     * @return bool.
      */
     public boolean isEmpty() {
         return first == null;
@@ -28,15 +79,17 @@ public class Stack<Item> implements Iterable<Item> {
 
    /**
      * Return the number of items in the stack.
+     * @return integer.
      */
     public int size() {
         return n;
     }
-
-   /**
-     * Add the item to the stack.
+    /**
+     * { function_description }.
+     *
+     * @param      item  The item
      */
-    public void push(Item item) {
+    public void push(final Item item) {
         Node oldfirst = first;
         first = new Node();
         first.item = item;
@@ -47,9 +100,12 @@ public class Stack<Item> implements Iterable<Item> {
    /**
      * Delete and return the item most recently added to the stack.
      * Throw an exception if no such item exists because the stack is empty.
+     * @return item.
      */
     public Item pop() {
-        if (isEmpty()) throw new RuntimeException("Stack underflow");
+        if (isEmpty()) {
+            throw new RuntimeException("Stack underflow");
+        }
         Item item = first.item;        // save item to return
         first = first.next;            // delete first node
         n--;
@@ -60,43 +116,77 @@ public class Stack<Item> implements Iterable<Item> {
    /**
      * Return the item most recently added to the stack.
      * Throw an exception if no such item exists because the stack is empty.
+     * @return item.
      */
     public Item peek() {
-        if (isEmpty()) throw new RuntimeException("Stack underflow");
+        if (isEmpty()) {
+            throw new RuntimeException("Stack underflow");
+        }
         return first.item;
     }
 
    /**
      * Return string representation.
+     * @return String.
      */
     public String toString() {
         StringBuilder s = new StringBuilder();
-        for (Item item : this)
+        for (Item item : this) {
             s.append(item + " ");
+        }
         return s.toString();
     }
-       
 
    /**
-     * Return an iterator to the stack that iterates through the items in LIFO order.
+     * Return an iterator to the stack
+     *  that iterates through the items in LIFO order.
+     *  @return item.
      */
-    public Iterator<Item> iterator()  { return new ListIterator();  }
+    public Iterator<Item> iterator()  {
+        return new ListIterator();
+    }
 
+    //
     // an iterator, doesn't implement remove() since it's optional
+    /**
+     *Iterator class.
+     */
     private class ListIterator implements Iterator<Item> {
+        /**
+         * current node.
+         */
         private Node current = first;
+        /**
+         * Determines if it has next.
+         *
+         * @return     True if has next, False otherwise.
+         */
         public boolean hasNext() {
             return current != null;
         }
+        /**
+         * no operation.
+         */
         public void remove() {
             throw new UnsupportedOperationException();
         }
 
+        /**
+         * next node item.
+         *
+         * @return     { description_of_the_return_value }
+         */
         public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
             Item item = current.item;
-            current = current.next; 
+            current = current.next;
             return item;
         }
     }
 }
+
+
+
+

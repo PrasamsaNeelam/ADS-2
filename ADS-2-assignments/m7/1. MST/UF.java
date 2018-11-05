bffe257efb8ee206e6f89b8e76c97850e9d85d32
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-11-06 00:34:04 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m7/1. MST/UF.java
+++ ADS-2-assignments/m7/1. MST/UF.java
@@ -1,19 +1,29 @@
+/**
+ * Class for uf.
+ */
 public class UF {
-
-    private int[] parent;  // parent[i] = parent of i
-    private byte[] rank;   // rank[i] = rank of subtree rooted at i (never more than 31)
-    private int count;     // number of components
+    /**
+     * { variable for parent array }.
+     */
+    private int[] parent;
+    /**
+     * { variable for rank array }.
+     */
+    private byte[] rank;
+    /**
+     * { variable for count }.
+     */
+    private int count;
 
     /**
-     * Initializes an empty union–find data structure with {@code n} sites
-     * {@code 0} through {@code n-1}. Each site is initially in its own 
-     * component.
+     * Constructs the object.
      *
-     * @param  n the number of sites
-     * @throws IllegalArgumentException if {@code n < 0}
+     * @param      n     { parameter_description }
      */
-    public UF(int n) {
-        if (n < 0) throw new IllegalArgumentException();
+    public UF(final int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
         count = n;
         parent = new int[n];
         rank = new byte[n];
@@ -24,16 +34,18 @@ public class UF {
     }
 
     /**
-     * Returns the component identifier for the component containing site {@code p}.
+     * Searches for the first match.
+     * its complexity is O(N)
      *
-     * @param  p the integer representing one site
-     * @return the component identifier for the component containing site {@code p}
-     * @throws IllegalArgumentException unless {@code 0 <= p < n}
+     * @param      p1     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
      */
-    public int find(int p) {
+    public int find(final int p1) {
+        int p = p1;
         validate(p);
         while (p != parent[p]) {
-            parent[p] = parent[parent[p]];    // path compression by halving
+            parent[p] = parent[parent[p]];
             p = parent[p];
         }
         return p;
@@ -41,56 +53,88 @@ public class UF {
 
     /**
      * Returns the number of components.
+     * its complexity is O(1)
      *
-     * @return the number of components (between {@code 1} and {@code n})
+     * @return the number of components
      */
     public int count() {
         return count;
     }
-  
+
     /**
      * Returns true if the the two sites are in the same component.
+     * its complexity is O(1)
      *
-     * @param  p the integer representing one site
-     * @param  q the integer representing the other site
-     * @return {@code true} if the two sites {@code p} and {@code q} are in the same component;
-     *         {@code false} otherwise
-     * @throws IllegalArgumentException unless
-     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+     * @param      p     { parameter_description }
+     * @param      q     The quarter
+     *
+     * @return     { description_of_the_return_value }
      */
-    public boolean connected(int p, int q) {
+    public boolean connected(final int p, final int q) {
         return find(p) == find(q);
     }
-  
+
     /**
-     * Merges the component containing site {@code p} with the 
-     * the component containing site {@code q}.
+     * Merges the component containing site p with the
+     * the component containing site q.
      *
-     * @param  p the integer representing one site
-     * @param  q the integer representing the other site
-     * @throws IllegalArgumentException unless
-     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+     * its complexity is O(1)
+     *
+     * @param      p     { parameter_description }
+     * @param      q     The quarter
      */
-    public void union(int p, int q) {
+    public void union(final int p, final int q) {
         int rootP = find(p);
         int rootQ = find(q);
-        if (rootP == rootQ) return;
-
+        if (rootP == rootQ) {
+            return;
+        }
         // make root of smaller rank point to root of larger rank
-        if      (rank[rootP] < rank[rootQ]) parent[rootP] = rootQ;
-        else if (rank[rootP] > rank[rootQ]) parent[rootQ] = rootP;
-        else {
+        if (rank[rootP] < rank[rootQ]) {
+            parent[rootP] = rootQ;
+        } else if (rank[rootP] > rank[rootQ]) {
+            parent[rootQ] = rootP;
+        } else {
             parent[rootQ] = rootP;
             rank[rootP]++;
         }
         count--;
     }
 
-    // validate that p is a valid index
-    private void validate(int p) {
+    /**
+     * validate that p is a valid index.
+     * its complexity is O(1)
+     *
+     * @param      p     { parameter_description }
+     */
+    private void validate(final int p) {
         int n = parent.length;
         if (p < 0 || p >= n) {
-            throw new IllegalArgumentException("index " + p + " is not between 0 and " + (n-1));  
+            throw new IllegalArgumentException("index " + p + " is not"
+                                               + "between 0 and " + (n - 1));
         }
     }
+
+    // /**
+    //  * Reads in a an integer {@code n} and a sequence of pairs of integers
+    //  * (between {@code 0} and {@code n-1}) from standard input,
+    //    where each integer
+    //  * in the pair represents some site;
+    //  * if the sites are in different components, merge the two components
+    //  * and print the pair to standard output.
+    //  *
+    //  * @param args the command-line arguments
+    //  */
+    // public static void main(String[] args) {
+    //     int n = StdIn.readInt();
+    //     UF uf = new UF(n);
+    //     while (!StdIn.isEmpty()) {
+    //         int p = StdIn.readInt();
+    //         int q = StdIn.readInt();
+    //         if (uf.connected(p, q)) continue;
+    //         uf.union(p, q);
+    //         StdOut.println(p + " " + q);
+    //     }
+    //     StdOut.println(uf.count() + " components");
+    // }
 }

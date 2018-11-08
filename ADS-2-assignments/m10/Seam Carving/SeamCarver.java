31a24a07ac3e610126601776b3e929de8226bb9a
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-11-08 14:01:59 +0530

    "ads2_m10_a1_1: 2/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m10/Seam Carving/SeamCarver.java
+++ ADS-2-assignments/m10/Seam Carving/SeamCarver.java
@@ -1,4 +1,4 @@
-
+import java.awt.Color;
 public class SeamCarver {
 	Picture picture;
 	// create a seam carver object based on the given picture
@@ -21,7 +21,19 @@ public class SeamCarver {
 
 	// energy of pixel at column x and row y
 	public double energy(int x, int y) {
-		return 0;
+		double deltax = 0.0;
+		double deltay = 0.0;
+		if (x == 0 || y == 0 || picture.height() - 1 == 0 || picture.width() - 1 == 0) {
+			return 1000;
+		} else {
+			Color hozleft = picture.get(x+1, y);
+			Color hozright = picture.get(x-1, y);
+			Color vertop = picture.get(x, y-1);
+			Color verbottom = picture.get(x, y+1);
+			deltax = (((hozleft.getRed() - hozright.getRed())^2) + ((hozleft.getGreen() - hozright.getGreen())^2) * ((hozleft.getBlue() - hozright.getBlue())^2));
+			deltay = (((vertop.getRed() - verbottom.getRed())^2) + ((vertop.getGreen() - verbottom.getGreen())^2) * ((vertop.getBlue() - verbottom.getBlue())^2));
+			return deltax + deltay;
+		}
 	}
 
 	// sequence of indices for horizontal seam

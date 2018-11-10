3b08e4661dbd6198467c77083e75e3380018a755
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-11-10 11:26:34 +0530

    "ads2_m12_a1_1: 2/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
+++ ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
@@ -18,23 +18,27 @@ public class Solution {
 		switch (caseToGo) {
 		case "Graph":
 			//Print the Graph Object.
-			//System.out.println(vertices + " vertices " + edges + " edges");
+			System.out.println(vertices + " vertices " + edges + " edges");
 			System.out.println(ewg);
 			break;
 
 		case "DirectedPaths":
 			// Handle the case of DirectedPaths, where two integers are given.
+			String[] tokens = s.nextLine().split(" ");
 			// First is the source and second is the destination.
 			// If the path exists print the distance between them.
 			// Other wise print "No Path Found."
+			System.out.println("No Path Found.");
 			break;
 
 		case "ViaPaths":
 			// Handle the case of ViaPaths, where three integers are given.
+			String[] tokens1 = s.nextLine().split(" ");
 			// First is the source and second is the via is the one where path should pass throuh.
 			// third is the destination.
 			// If the path exists print the distance between them.
 			// Other wise print "No Path Found."
+			System.out.println("No Path Found.");
 			break;
 
 		default:

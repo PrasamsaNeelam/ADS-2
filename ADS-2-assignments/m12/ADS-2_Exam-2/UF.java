467c915b3357aedca84b34278bfab4c9215e4ac8
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-11-10 11:03:34 +0530

    "ads2_m12_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ ADS-2-assignments/m12/ADS-2_Exam-2/UF.java
@@ -0,0 +1,117 @@
+/**
+ * Class for uf.
+ */
+public class UF {
+    /**
+     * { variable for parent array }.
+     */
+    private int[] parent;
+    /**
+     * { variable for rank array }.
+     */
+    private byte[] rank;
+    /**
+     * { variable for count }.
+     */
+    private int count;
+
+    /**
+     * Constructs the object.
+     *
+     * @param      n     { parameter_description }
+     */
+    public UF(final int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
+        count = n;
+        parent = new int[n];
+        rank = new byte[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    /**
+     * Searches for the first match.
+     * its complexity is O(N)
+     *
+     * @param      p1     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int find(final int p1) {
+        int p = p1;
+        validate(p);
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+        return p;
+    }
+
+    /**
+     * Returns the number of components.
+     * its complexity is O(1)
+     *
+     * @return the number of components
+     */
+    public int count() {
+        return count;
+    }
+
+    /**
+     * Returns true if the the two sites are in the same component.
+     * its complexity is O(1)
+     *
+     * @param      p     { parameter_description }
+     * @param      q     The quarter
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean connected(final int p, final int q) {
+        return find(p) == find(q);
+    }
+
+    /**
+     * Merges the component containing site p with the
+     * the component containing site q.
+     *
+     * its complexity is O(1)
+     *
+     * @param      p     { parameter_description }
+     * @param      q     The quarter
+     */
+    public void union(final int p, final int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) {
+            return;
+        }
+        // make root of smaller rank point to root of larger rank
+        if (rank[rootP] < rank[rootQ]) {
+            parent[rootP] = rootQ;
+        } else if (rank[rootP] > rank[rootQ]) {
+            parent[rootQ] = rootP;
+        } else {
+            parent[rootQ] = rootP;
+            rank[rootP]++;
+        }
+        count--;
+    }
+
+    /**
+     * validate that p is a valid index.
+     * its complexity is O(1)
+     *
+     * @param      p     { parameter_description }
+     */
+    private void validate(final int p) {
+        int n = parent.length;
+        if (p < 0 || p >= n) {
+            throw new IllegalArgumentException("index " + p + " is not"
+                                               + "between 0 and " + (n - 1));
+        }
+    }
+}

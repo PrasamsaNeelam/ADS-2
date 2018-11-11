e9b4733a2432d0d6450cd8db9e1fb68fcd56daa2
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-11-11 23:38:45 +0530

    "ads2_m10_a1_1: 9/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m10/Seam Carving/ShowEnergy.java
+++ ADS-2-assignments/m10/Seam Carving/ShowEnergy.java
@@ -11,18 +11,29 @@
 
 import edu.princeton.cs.algs4.Picture;
 import edu.princeton.cs.algs4.StdOut;
-
+/**
+ * Class for show energy.
+ */
 public class ShowEnergy {
+	/**
+	 * Constructs the object.
+	 */
+	private ShowEnergy() {
 
+	}
+	/**
+	 * Main function.
+	 *
+	 * @param      args  The arguments
+	 */
     public static void main(String[] args) {
         Picture picture = new Picture(args[0]);
-        StdOut.printf("image is %d columns by %d rows\n", picture.width(), picture.height());
+        StdOut.printf("image is %d columns by %d rows\n",
+        	picture.width(), picture.height());
         picture.show();        
         SeamCarver sc = new SeamCarver(picture);
         
         StdOut.printf("Displaying energy calculated for each pixel.\n");
         SCUtility.showEnergy(sc);
-
     }
-
 }

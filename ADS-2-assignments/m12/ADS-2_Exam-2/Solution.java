2472673a90a5bebdeae52bbdafb447e44cb14d64
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-11-11 15:56:47 +0530

    "ads2_m12_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
+++ ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
@@ -59,14 +59,70 @@ public final class Solution {
             break;
 
         case "ViaPaths":
-            // Handle the case of ViaPaths, where three integers are given.
-            String[] tokens1 = s.nextLine().split(" ");
-            // First is the source and second is the via is the
-            // one where path should pass throuh.
-            // third is the destination.
-            // If the path exists print the distance between them.
-            // Other wise print "No Path Found."
-            System.out.println("No Path Found.");
+            String[] paths = s.nextLine().split(" ");
+            int source = Integer.parseInt(paths[0]);
+            int via = Integer.parseInt(paths[1]);
+            int destination = Integer.parseInt(paths[2]);
+            String str = "";
+
+            DijkstraUndirectedSP sp1 =
+                new DijkstraUndirectedSP(ewg, source);
+            if (sp1.hasPathTo(destination)) {
+                // str1 = sp1.pathTo(d1).toString();
+                Queue<Integer> queue = new Queue<Integer>();
+                for (Edge edge : sp1.pathTo(via)) {
+                    int vertex = edge.either();
+                    int other = edge.other(vertex);
+                    int count1 = 0;
+                    int count2 = 0;
+                    for (Integer i : queue) {
+                        if (vertex == i) {
+                            count1 = 1;
+                        }
+                        if (other == i) {
+                            count2 = 1;
+                        }
+                    }
+                    if (count2 == 0) {
+                        queue.enqueue(other);
+                    }
+                    if (count1 == 0) {
+                        queue.enqueue(vertex);
+                    }
+                }
+                DijkstraUndirectedSP sp2 =
+                    new DijkstraUndirectedSP(ewg, via);
+                for (Edge edge : sp2.pathTo(destination)) {
+                    int vertex = edge.either();
+                    int other = edge.other(vertex);
+                    int count1 = 0;
+                    int count2 = 0;
+                    for (Integer i : queue) {
+                        if (vertex == i) {
+                            count1 = 1;
+                        }
+                        if (other == i) {
+                            count2 = 1;
+                        }
+
+                    }
+                    if (count1 == 0) {
+                        queue.enqueue(vertex);
+                    }
+                    if (count2 == 0) {
+                        queue.enqueue(other);
+                    }
+                }
+                System.out.println(sp1.distTo(via)
+                                   + sp2.distTo(destination));
+                while (!queue.isEmpty()) {
+                    str += queue.dequeue() + " ";
+                }
+                System.out.print(str.trim());
+            } else {
+                System.out.println("No Path Found.");
+            }
+            // System.out.println(dist1 + dist2);
             break;
 
         default:

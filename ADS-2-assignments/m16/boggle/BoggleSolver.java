3f19401f70c10e3cbb28145873d3fd49d622b89e
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-11-15 12:35:19 +0530

    "ads2_m16_a1_1: 2/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m16/boggle/BoggleSolver.java
+++ ADS-2-assignments/m16/boggle/BoggleSolver.java
@@ -1,8 +1,12 @@
 public class BoggleSolver {
 	// Initializes the data structure using the given array of strings as the dictionary.
+	TST<Integer> tst;
 	// (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
 	public BoggleSolver(String[] dictionary) {
-
+		tst = new TST<Integer>();
+		for (int i = 0; i < dictionary.length; i++) {
+			tst.put(dictionary[i], i);
+		}
 	}
 
 	// Returns the set of all valid words in the given Boggle board, as an Iterable.
@@ -13,6 +17,18 @@ public class BoggleSolver {
 	// Returns the score of the given word if it is in the dictionary, zero otherwise.
 	// (You can assume the word contains only the uppercase letters A through Z.)
 	public int scoreOf(String word) {
-		return 0;
+		if (word.length() > 8) {
+			return 11;
+		} else if (word.length() == 7) {
+			return 5;
+		} else if (word.length() == 6) {
+			return 3;
+		} else if (word.length() == 5) {
+			return 2;
+		} else if (word.length() == 3 || word.length() == 4) {
+			return 1;
+		} else {
+			return 0;
 	}
+}
 }
